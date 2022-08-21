98b2ad049668440a8cac0ce4edca8d0d9f4d7ade
2loopVoyvoda <94403004+2loopVoyvoda@users.noreply.github.com>
2022-08-21 16:41:09 +0300

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: [
+        'plugin:vue/recommended'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    plugins: ['vue'],
+    rules: {
+        'no-unused-vars': 'off',
+        'vue/no-side-effects-in-computed-properties': 'off',
+    },
+};
